bc7f79f1385ccacd34e4e0b009370e70d854c489
Chris <shockingchris110@gmail.com>
2016-10-02 10:36:49 -0600

    added survey pages and updated index and styling

---
+++ web/survey.php
@@ -0,0 +1,99 @@
+<?php
+	session_start();
+	
+	if(isset($_SESSION['taken'])){
+		header("Location: /surveyresults.php");
+	}
+?>
+<html lang="en">
+	<head>
+		<title>Survey Page</title>
+		<link rel="stylesheet" href="intro.css"/>
+    </head>
+    <body>
+		<div class="top">
+			<ul class="topbar">
+				<li class="toplist"><a href="homepage.html">Home Page</a></li>
+				<li class="toplist"><a>Games</a></li>
+				<li style="float:right" class="toplist"><a>Login</a></li>
+            </ul>
+		</div>
+	
+		<div class="left">
+			<ul class="leftbar">
+				<li class="leftlist"><a href="index.html">Assignments Page</a></li>
+				<li class="leftlist"><a href="survey.php" class="active">Week 02</a></li>
+				<li class="leftlist"><a>Week 03</a></li>
+				<li class="leftlist"><a>Week 04</a></li>
+				<li class="leftlist"><a>Week 05</a></li>
+				<li class="leftlist"><a>Week 06</a></li>
+				<li class="leftlist"><a>Week 07</a></li>
+				<li class="leftlist"><a>Week 08</a></li>
+				<li class="leftlist"><a>Week 09</a></li>
+				<li class="leftlist"><a>Week 10</a></li>
+				<li class="leftlist"><a>Week 11</a></li>
+				<li class="leftlist"><a>Week 12</a></li>
+				<li class="leftlist"><a>Week 13</a></li>
+			</ul>
+		</div>
+		<div class="main">
+		<form action="surveyresults.php" method="POST">
+			<h2>Survey</h2>
+			<br>
+			
+			Name: <input type="text" name="name">
+			<br><br>
+			
+			Question 1:
+			<br>
+			<input type="radio" name="phone" value="Apple">Apple
+			<br>
+			<input type="radio" name="phone" value="Android">Android
+			<br>
+			<input type="radio" name="phone" value="Windows">Windows
+			<br>
+			<input type="radio" name="phone" value="Other">Other
+			<br><br>
+			
+			Question 2: Are you happy with your phone?
+			<br>
+			<input type="radio" name="happy" value="happy">Yes
+			<br>
+			<input type="radio" name="happy" value="unhappy">No
+			<br>
+			<br><br>
+			
+			Question 3: What type of movies do you like? (check all that apply)
+			<br>
+			<input type="checkbox" name="movies[]" value="Scary Movies">Scary Movies
+			<br>
+			<input type="checkbox" name="movies[]" value="Chick Flick">Chick Flick
+			<br>
+			<input type="checkbox" name="movies[]" value="Suspense">Suspense
+			<br>
+			<input type="checkbox" name="movies[]" value="Comedy">Comedy
+			<br>
+			<input type="checkbox" name="movies[]" value="Action">Action
+			<br>
+			<input type="checkbox" name="movies[]" value="Disney">Disney
+			<br><br>
+			
+			Question 4: Did you enjoy this survey?
+			<br>
+			<input type="radio" name="enjoy" value="Yes" checked="true">Yes
+			<br>
+			<br><br>
+			
+			Add any comments you have about this survey: (50 Characters Maximum)
+			<br>
+			<textarea name="comment" rows="5" cols="40" maxlength="50" value=""></textarea>
+			<br><br>
+			
+			<input type="submit" name="submit" value="Submit">
+			<a href="surveyresults.php">    Go Straight to results</a>
+			
+			<br><br><br><br><br><br>
+		</form>
+		</div>
+	</body>
+</html>
---
+++ web/surveyresults.php
@@ -0,0 +1,120 @@
+<?php
+	session_start();
+?>
+<html lang="en">
+	<head>
+		<title>Results</title>
+		<link rel="stylesheet" href="intro.css"/>
+    </head>
+    <body>
+		<div class="top">
+			<ul class="topbar">
+				<li class="toplist"><a href="homepage.html">Home Page</a></li>
+				<li class="toplist"><a>Games</a></li>
+				<li style="float:right" class="toplist"><a>Login</a></li>
+            </ul>
+		</div>
+	
+		<div class="left">
+			<ul class="leftbar">
+				<li class="leftlist"><a>Assignments Page</a></li>
+				<li class="leftlist"><a href="survey.php" class="active">Week 02</a></li>
+				<li class="leftlist"><a>Week 03</a></li>
+				<li class="leftlist"><a>Week 04</a></li>
+				<li class="leftlist"><a>Week 05</a></li>
+				<li class="leftlist"><a>Week 06</a></li>
+				<li class="leftlist"><a>Week 07</a></li>
+				<li class="leftlist"><a>Week 08</a></li>
+				<li class="leftlist"><a>Week 09</a></li>
+				<li class="leftlist"><a>Week 10</a></li>
+				<li class="leftlist"><a>Week 11</a></li>
+				<li class="leftlist"><a>Week 12</a></li>
+				<li class="leftlist"><a>Week 13</a></li>
+			</ul>
+		</div>
+		<div class="main">
+		<pre>
+		<?php
+		//printing out results for use
+			//print_r($_POST);
+			
+			//Survey questions: 
+			$_SESSION['taken'] = 0;
+			//set default values
+			if (isset($_POST['submit'])) {
+				$name = isset($_POST['name']) ? $_POST['name'] : "";
+				$phone = isset($_POST['phone']) ? $_POST['phone'] : "";
+				$happy = isset($_POST['happy']) ? $_POST['happy'] : "";
+				$movies = isset($_POST['movies']) ? $_POST['movies'] : "";
+				$enjoy = isset($_POST['enjoy']) ? $_POST['enjoy'] : "";
+				$comment = isset($_POST['comment']) ? $_POST['comment'] : "";
+			} else {
+				//otherwise set to empty.
+				$name = "";
+				$phone = "";
+				$happy = "";
+				$movies = "";
+				$enjoy = "";
+				$comment = "";
+			}
+			
+			if ($_SESSION['taken'] == 0){
+				$_SESSION['name'] = $name;
+				$_SESSION['phone'] = $phone;
+				$_SESSION['happy'] = $happy;
+				$_SESSION['movies'] = $movies;
+				$_SESSION['enjoy'] = $enjoy;
+				$_SESSION['comment'] = $comment;
+				$_SESSION['taken'] = 1;
+			}
+			
+			if (!isset($_SESSION['name'])){
+				$_SESSION['taken'] = 0;
+			}
+			
+			if($_SESSION['taken'] == 1){
+				$myfile = fopen("surveys.txt", "w+") or die("Unable to open file!");
+				fwrite($myfile, "Survey for {$name}:");
+				if(!empty($phone)){
+					fwrite($myfile,$phone);
+				}
+				if(!empty($happy)){
+					fwrite($myfile,$happy);
+				}
+				if (!empty($movies)){
+					foreach($movies as $i){
+						fwrite($myfile,$i);
+					}
+				}
+				if(!empty($enjoy)){
+					fwrite($myfile,$enjoy);
+				}
+				if(!empty($enjoy)){
+					fwrite($myfile,$comment);
+				}
+				fclose($myfile);
+			}
+		?>
+		</pre>
+		<?php
+		//print out what the values are
+			if($_SESSION['taken'] == 1){
+				echo "You've finished your surveys now.<br>";
+				echo "Here were your results: <br><br>";
+			}
+			echo "Hello {$_SESSION['name']}! <br>";
+			echo "From our survey, you use a {$_SESSION['phone']} Phone.<br>";
+			echo "You seem to be {$_SESSION['happy']} about it.<br>";
+			echo "If you have any movie favorites, they are listed here: <br>";
+			if (!empty($_SESSION['movies'])){
+				foreach($_SESSION['movies'] as $i){
+					echo "{$i} <br>";
+				}
+			}
+			echo "We're glad you enjoyed this Survey! <br>";
+			echo "Your results were saved. <br>";
+			echo "Have a wonderful day!";
+		?>
+		</div>
+	</body>
+</html>

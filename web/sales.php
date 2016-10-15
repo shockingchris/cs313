462d7956dd777adb17fcfa26449e110d74afc413
Chris <shockingchris110@gmail.com>
2016-10-15 16:13:07 -0600

    added table in, trying to populate with database stuff

--- web/sales.php
+++ web/sales.php
@@ -2,12 +2,53 @@
 <html>
 <head>
 	<link rel="stylesheet" href="/intro.css"/>
+	<link rel="stylesheet" href="/table.css"/>
 </head>
 <body>
 <div class="top"><?php include 'topnavbars.php';?></div>
 <div class="left"><?php include 'leftnavbars.php';?></div>
 <div class="main">
 <h1>Salesman Information</h1>
+
+<table class="responstable">
+  
+  <tr>
+    <th>Salesperson</th>
+    <th>Calls (15)</th>
+    <th>Appointments (15)</th>
+	<th></th>
+  </tr>
+  
+  <tr>
+    <td><?php $db->query('SELECT name FROM salesman WHERE user_id=1')?></td>
+    <td></td>
+    <td>Salesman</td>
+	<td>Yes</td>
+  </tr>
+  
+  <tr>
+    <td><?php $db->query('SELECT name FROM salesman WHERE user_id=1')?></td>
+    <td>01/01/1985</td>
+    <td>Salesman</td>
+	<td>Yes</td>
+  </tr>
+  
+  <tr>
+    <td>Kelsi Simmons</td>
+    <td>01/01/1990</td>
+    <td>Saleswoman</td>
+	<td>No</td>
+  </tr>
+  
+  <tr>
+    <td>Angie Simmons</td>
+    <td>01/01/1990</td>
+    <td>Saleswoman</td>
+	<td>No</td>
+  </tr>
+  
+</table>
+
 <?php include 'queries.php';?>
 </div>
 </body>

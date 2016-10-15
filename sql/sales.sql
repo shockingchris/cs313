967497149e86c1d0cbfc82f992fa264d88d15f2d
Chris <shockingchris110@gmail.com>
2016-10-14 21:47:36 -0600

    added sales.php and sales.sql

---
+++ sql/sales.sql
@@ -0,0 +1,50 @@
+CREATE TABLE salesman(
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL);
+
+CREATE TABLE appt(
+id SERIAL PRIMARY KEY,
+info TEXT NOT NULL,
+pointval INT NOT NULL,
+user_id INT REFERENCES salesman(id)
+);
+
+CREATE TABLE call(
+id SERIAL PRIMARY KEY,
+info TEXT NOT NULL,
+pointval INT NOT NULL,
+user_id INT REFERENCES salesman(id)
+);
+
+CREATE TABLE deal(
+id SERIAL PRIMARY KEY,
+info TEXT NOT NULL,
+pointval INT NOT NULL,
+user_id INT REFERENCES salesman(id)
+);
+
+INSERT INTO salesman
+VALUES ('Joel Simmons');
+INSERT INTO salesman
+VALUES ('Jeff Simmons');
+
+INSERT INTO deal (info, pointval, user_id)
+VALUES ('sold a car', 15, 1);
+INSERT INTO deal (info, pointval, user_id)
+VALUES ('sold a house', 15, 1);
+INSERT INTO deal (info, pointval, user_id)
+VALUES ('sold a boat', 15, 2);
+
+INSERT INTO appt (info, pointval, user_id)
+VALUES ('set an appt - Friday', 15, 2);
+INSERT INTO appt (info, pointval, user_id)
+VALUES ('set an appt - Thursday', 15, 1);
+INSERT INTO appt (info, pointval, user_id)
+VALUES ('set an appt - Sunday', 15, 1);
+
+INSERT INTO call (info, pointval, user_id)
+VALUES ('call made - Thursday', 15, 1);
+INSERT INTO call (info, pointval, user_id)
+VALUES ('call made - Sunday', 15, 2);
+INSERT INTO call (info, pointval, user_id)
+VALUES ('call made - Sunday', 15, 1);

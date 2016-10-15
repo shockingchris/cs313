6942089df34b81c1f314d1c98bcbab2f57b97663
Chris <shockingchris110@gmail.com>
2016-10-15 08:34:06 -0600

    added navbars straight

--- web/sales.php
+++ web/sales.php
@@ -4,7 +4,31 @@
 </head>
 <body>
 
-<?php include 'navbars.php'; ?>
+<div class="top">
+	    <ul class="topbar">
+            <li class="toplist"><a href="homepage.html">Home Page</a></li>
+            <li class="toplist"><a>Games</a></li>
+            <li style="float:right" class="toplist"><a>Login</a></li>
+            </ul>
+    </div>
+	
+	<div class="left">
+        <ul class="leftbar">
+            <li class="leftlist"><a class="active">Assignments Page</a></li>
+            <li class="leftlist"><a href="survey.php">Week 02</a></li>
+			<li class="leftlist"><a>Week 03</a></li>
+			<li class="leftlist"><a>Week 04</a></li>
+			<li class="leftlist"><a>Week 05</a></li>
+			<li class="leftlist"><a>Week 06</a></li>
+			<li class="leftlist"><a>Week 07</a></li>
+			<li class="leftlist"><a>Week 08</a></li>
+			<li class="leftlist"><a>Week 09</a></li>
+			<li class="leftlist"><a>Week 10</a></li>
+			<li class="leftlist"><a>Week 11</a></li>
+			<li class="leftlist"><a>Week 12</a></li>
+			<li class="leftlist"><a>Week 13</a></li>
+        </ul>
+    </div>
 <div class="main">
 <h1>Salesman Information</h1>
 <?php

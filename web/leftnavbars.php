f20fab08aba77540da6c3e1d20abc6b70d07b84a
Chris <shockingchris110@gmail.com>
2016-10-16 16:51:35 -0600

    trying to get values into table

--- web/leftnavbars.php
+++ web/leftnavbars.php
@@ -2,7 +2,7 @@
     echo'<ul class="leftbar">';
     echo'<li class="leftlist"><a href="index.html">Assignments Page</a></li>';
     echo'<li class="leftlist"><a href="survey.php">Survey</a></li>';
-	echo'<li class="leftlist"><a href="sales.php" class="active">Sales table</a></li>';
+	echo'<li class="leftlist"><a href="sales.php" class="active">Sales Table</a></li>';
 	echo'<li class="leftlist"><a>Week 04</a></li>';
 	echo'<li class="leftlist"><a>Week 05</a></li>';
 	echo'<li class="leftlist"><a>Week 06</a></li>';

332443a0f725911f029096eada5b2b4b18650804
Chris <shockingchris110@gmail.com>
2016-10-16 11:47:15 -0600

    trying to get values into table

--- web/queries.php
+++ web/queries.php
@@ -1,12 +1,12 @@
 <?php
-foreach ($db->query('SELECT salesman.name, appt.pointval, call.pointval, deal.pointval
+foreach ($db->query('SELECT salesman.name, appt.apptval, call.callval, deal.dealval
 FROM salesman, appt, call, deal LIMIT 2') as $row)
 {
 	echo '<tr>';
 	echo '<td>' . $row['name'] . '</td>';
-	echo '<td>' . $row['pointval'] . '</td>';
-	echo '<td>' . $row['pointval'] . '</td>';
-	echo '<td>' . $row['pointval'] . '</td>';
+	echo '<td>' . $row['callval'] . '</td>';
+	echo '<td>' . $row['apptval'] . '</td>';
+	echo '<td>' . $row['dealval'] . '</td>';
 	echo '</tr>';
 }
 	// foreach ($db->query('SELECT info FROM deal') as $row)

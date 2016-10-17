010cb450c63dbcb8d2af6a9e164a671b18da1358
Chris <shockingchris110@gmail.com>
2016-10-16 19:00:04 -0600

    Added scriptures.php for team assignment

--- web/scriptures.php
+++ web/scriptures.php
@@ -11,16 +11,18 @@ Enter a new Scripture to add:<br>
 <input type="text" name="book" size="15"/>
 <input type="text" name="chapter" maxlength="3" size="3"/>
 : <input type="text" name="verse" maxlength="3" size="3"/>
-<br><textarea rows="4" cols="40"/>
+<br><textarea rows="4" cols="40"></textarea>
+<br/>
 <?php
 
-foreach ($db->query("SELECT book, chapter, verse, content FROM scripture") as $scripture) {
-	echo "<b>" . $scripture['book'] . ' ';
-	echo $scripture['chapter'] . ':';
-	echo $scripture['verse'] . "</b>";
-	echo " - \"" . $scripture['content'] . "\"<br><br>";
+foreach ($db->query("SELECT book, chapter, verse, content FROM scripture") as $row) {
+	echo "<b>" . $row['book'] . ' ';
+	echo $row['chapter'] . ':';
+	echo $row['verse'] . "</b>";
+	echo " - \"" . $row['content'] . "\"<br><br>";
 }
 ?>
+
 </body>
 </html>   
 

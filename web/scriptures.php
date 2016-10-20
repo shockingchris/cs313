e5b9297a9451e1fe1fd00a589aa5a803a1aa189c
Chris <shockingchris110@gmail.com>
2016-10-19 20:17:03 -0600

    adding in insert into scripture table from php

--- web/scriptures.php
+++ web/scriptures.php
@@ -15,10 +15,10 @@
 		$topics='';
 	}
 
-	// if(isset($_POST['submit'])){
-		// foreach($db->query('INSERT INTO scripture(book, chapter, verse, content)
-							// VALUES("$book", "$chapter", "$verse", "$content");'));
-		// }
+	if(isset($_POST['submit'])){
+		foreach($db->query('INSERT INTO scripture(book, chapter, verse, content)
+						VALUES("$book", "$chapter", "$verse", "$content");'));
+		}
 ?>
 <html>
 <head>

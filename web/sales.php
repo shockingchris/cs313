6091f66b30af3a53a49071e9c83d8745958e2bc6
Chris <shockingchris110@gmail.com>
2016-10-20 17:43:55 -0600

    adding a new person form

--- web/sales.php
+++ web/sales.php
@@ -1,4 +1,32 @@
 <?php include 'dbaccess.php';?>
+<?php
+	print_r($_POST);
+	if(isset($_POST['submit'])){
+		//echo "We made it submit!";
+		$salesman = isset($_POST['salesman']) ? $_POST['salesman'] : '';
+		$newperson = isset($_POST['newperson']) ? $_POST['newperson'] : ''; 
+		$incentive = isset($_POST['incentive']) ? $_POST['incentive'] : ''; 
+		$newtopic = isset($_POST['newtopic']) ? $_POST['newtopic'] : '';
+	}
+	else{
+		//echo "didn't submit";
+		$salesman = '';
+		$newperson ='';
+		$incentive ='';
+		$newtopic='';
+		$newId='';
+		$newTopicId='';
+	}
+	
+	if($_POST['newperson']!=''){
+		$stmt = $db->prepare("INSERT INTO salesman(name)
+						VALUES(:name)");
+		$stmt->bindParam(":name", $newperson, PDO::PARAM_STR, 100);
+		$stmt->execute();
+	}
+	
+	$newTopicId = $db->lastInsertId();
+?>
 <html>
 <head>
 	<link rel="stylesheet" href="/intro.css"/>
@@ -15,32 +43,29 @@
   <tr>
     <th>Salesperson</th>
     <th>Calls (15)</th>
-    <th>Appointments (15)</th>
+    <th>Appointments(15)</th>
 	<th>Deals (15)</th>
 	<th>Total</th>
   </tr>
 
-<?php include 'queries.php';?>
-
-  <tr>
-    <td>Kelsi Simmons</td>
-    <td>15</td>
-    <td>15</td>
-	<td>15</td>
-	<td>45</td>
-  </tr>
-  
-  <tr>
-    <td>Angie Simmons</td>
-    <td>15</td>
-    <td>15</td>
-	<td>15</td>
-	<td>45</td>
-  </tr>
+<?php
+foreach ($db->query('SELECT salesman.name, appt.apptval, call.callval, deal.dealval
+FROM salesman, appt, call, deal LIMIT 2') as $row)
+{
+	echo '<tr>';
+	echo '<td>' . $row['name'] . '</td>';
+	echo '<td>' . $row['callval'] . '</td>';
+	echo '<td>' . $row['apptval'] . '</td>';
+	echo '<td>' . $row['dealval'] . '</td>';
+	$total = 0;
+	$total = $row['callval'] + $row['apptval'] + $row['dealval'];
+	echo '<td>' . $total . '</td>';
+	echo '</tr>';
+}
+?>
   
 </table>
 <br/>
-<form action="" method="POST">
 Following Salesman available to search info:<br>
 <?php
 	foreach($db->query("SELECT name FROM salesman") as $row)
@@ -48,34 +73,47 @@ Following Salesman available to search info:<br>
 			echo $row['name'] . '<br>';
 		}
 ?>
+
+<br/>
+Add a new Salesman: 
+<form action='' method="POST">
+<input type="text" name="newperson"/>
+<button type="submit" name="submit" value="submit">Submit</button>
+</form>
+
 <br>See a salesman's sales info: <br/>
+<form action="" method="POST">
 <input type="text" name="salesname"/><br/>
 <button type="submit" name="submit" value="submit">Submit</button>
+</form>
+
 <?php 
 	if(isset($_POST['salesname'])){
 		$salesname = $_POST['salesname'];
 		echo "<br>Information for {$salesname}:<br>";
 		echo "Appointments:<br>";
 		foreach($db->query("SELECT salesman.name, appt.ainfo FROM
-	salesman INNER JOIN appt ON salesman.id=appt.user_id WHERE salesman.name='$salesname'") as $row)
+				salesman INNER JOIN appt ON salesman.id=appt.user_id
+				WHERE salesman.name='$salesname'") as $row)
 		{
 			echo $row['ainfo'] . '<br>';
 		}
 		echo "<br>Calls:<br>";
 		foreach($db->query("SELECT salesman.name, call.cinfo FROM
-		salesman INNER JOIN call ON salesman.id=call.user_id WHERE salesman.name='$salesname'") as $row)
+				salesman INNER JOIN call ON salesman.id=call.user_id
+				WHERE salesman.name='$salesname'") as $row)
 		{
 			echo $row['cinfo'] . '<br>';
 		}
 		echo "<br>Deals Closed:<br>";
 		foreach($db->query("SELECT salesman.name, deal.dinfo FROM
-		salesman INNER JOIN deal ON salesman.id=deal.user_id WHERE salesman.name='$salesname'") as $row)
+				salesman INNER JOIN deal ON salesman.id=deal.user_id
+				WHERE salesman.name='$salesname'") as $row)
 		{
 			echo $row['dinfo'] . '<br>';
 		}
 	}
 ?>
-</form>
 </div>
 </body>
 </html>

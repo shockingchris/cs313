06998813c726c8441f13cd576960a6670e58426b
Chris <shockingchris110@gmail.com>
2016-10-19 22:10:52 -0600

    trying to insert STUFF

--- web/scriptures.php
+++ web/scriptures.php
@@ -52,8 +52,8 @@
 	if (!empty($topics)){
 		echo "In the link IF";
 			foreach($topics as $row){
-				echo "in the link FOREACH";
-				$topicId = $db->query('SELECT id FROM topics WHERE name="$row"');
+				echo "in the link FOREACH $";
+				$topicId = $db->query("SELECT id FROM topics WHERE name='$row'");
 				echo "<br>$topicId";
 				$stmt = $db->prepare("INSERT INTO link(scripture_id, topics_id)
 				VALUES(:scripture_id, :topics_id)");

786616a9b50a87ddf030a9cee6f8fb973c67a871
Chris <shockingchris110@gmail.com>
2016-10-19 20:22:51 -0600

    adding in insert into scripture table from php

--- web/scriptures.php
+++ web/scriptures.php
@@ -34,7 +34,7 @@ Enter a new Scripture to add:<br>
 <input type="text" name="book" size="15"/>
 <input type="text" name="chapter" maxlength="3" size="3"/>
 : <input type="text" name="verse" maxlength="3" size="3"/>
-<br><textarea rows="4" cols="40"></textarea>
+<br><textarea name="content" rows="4" cols="40"></textarea>
 <br/>
 Topics:  <br>
 <?php

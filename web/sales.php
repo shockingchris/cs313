b57ff6a7233e01e523f0c19e0057b551e5f9a2b6
Chris <shockingchris110@gmail.com>
2016-10-22 13:49:04 -0600

    adding delete of person not gonna work

--- web/sales.php
+++ web/sales.php
@@ -20,7 +20,7 @@
 	}
 	
 	if($submit == 'deletePerson'){
-		$stmt = $db->prepare("DELETE FROM salesman WHERE name='$deletedPerson'");
+		$stmt = $db->prepare("DELETE FROM salesman WHERE name=':deletedPerson'");
 		$stmt->bindparam(":name", $deletedPerson, PDO:PARAM_STR, 100);
 		$stmt->execute();
 	}

53e3dff7d0cab432715294f8c9a25f74814177fb
Chris <shockingchris110@gmail.com>
2016-10-23 14:32:08 -0600

    adding sign in page

--- web/signin.php
+++ web/signin.php
@@ -1,4 +1,7 @@
 <?php include 'dbaccess.php';?>
+<?php 
+	print_r($_POST);
+?>
 <html>
   <head>
 <head>
@@ -7,32 +10,7 @@
 	<link rel="stylesheet" href="/table.css"/>
 </head>
   <body>
-<!--	<div class="top">
-	    <ul class="topbar">
-            <li class="toplist"><a href="homepage.html">Home Page</a></li>
-            <li class="toplist"><a>Games</a></li>
-            <li style="float:right" class="toplist" class="active"><a href="signin.php">Login</a></li>
-            </ul>
-    </div>
-	<div class="left">
-        <ul class="leftbar">
-            <li class="leftlist"><a href="index.html">Assignments Page</a></li>
-            <li class="leftlist"><a href="survey.php">Survey</a></li>
-			<li class="leftlist"><a href="sales.php">Sales Table</a></li>
-			<li class="leftlist"><a>Week 04</a></li>
-			<li class="leftlist"><a>Week 05</a></li>
-			<li class="leftlist"><a>Week 06</a></li>
-			<li class="leftlist"><a>Week 07</a></li>
-			<li class="leftlist"><a>Week 08</a></li>
-			<li class="leftlist"><a>Week 09</a></li>
-			<li class="leftlist"><a>Week 10</a></li>
-			<li class="leftlist"><a>Week 11</a></li>
-			<li class="leftlist"><a>Week 12</a></li>
-			<li class="leftlist"><a>Week 13</a></li>
-        </ul>
-    </div>  -->
-		<h1>Sign In Here</h1>
-		<br>
+		<br><br><br>
 		<form action='' method='POST'>
 		<table align="center" class="responstable">
 		<tr>
@@ -43,7 +21,10 @@
 		</tr><tr>
 		<td>Password:</td>
 		<td><input type="password" name="pass" size="15"/></td>
-		</tr>
+		</tr><tr>
+		<td colspan="2">
+		<input type="submit" name="submit" value="login">Log In</input>
+		</td>
 		</table>
 		</form>
 	

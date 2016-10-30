0fccb1222bddbd5ce0c55610df4e3aa92aeba3ed
Chris <shockingchris110@gmail.com>
2016-10-29 19:43:08 -0600

    added header statements to send client to sign in page

--- web/survey.php
+++ web/survey.php
@@ -15,7 +15,7 @@
 			<ul class="topbar">
 				<li class="toplist"><a href="homepage.php">Home Page</a></li>
 				<li class="toplist"><a>Games</a></li>
-				<li style="float:right" class="toplist"><a>Login</a></li>
+				<li style="float:right" class="toplist"><a href="signin.php">Login</a></li>
             </ul>
 		</div>
 	
--- web/topnavbars.php
+++ web/topnavbars.php
@@ -2,6 +2,6 @@
     echo'<ul class="topbar">';
     echo'<li class="toplist"><a href="homepage.php">Home Page</a></li>';
     echo'<li class="toplist"><a>Games</a></li>';
-    echo'<li style="float:right" class="toplist"><a>Login</a></li>';
+    echo'<li style="float:right" class="toplist"><a href="signin.php">Login</a></li>';
     echo'</ul>';
 ?>
